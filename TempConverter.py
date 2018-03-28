a81db4c2188897236c6bb3116a6d452dbb13a54f
Bianca <beeankha@gmail.com>
2018-03-28 10:50:24 -0400

    Add files via upload

---
+++ TempConverter.py
@@ -0,0 +1,9 @@
+x=int(32)
+y=int(5)
+z=int(9)
+
+print('Let\'s figure out the temperature in celsius!')
+
+f = int(input('What is the temperature in farenheit?'))
+c=int((f - x) * y / z)
+print('The temperature in celsius is', c)
