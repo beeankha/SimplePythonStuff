d9abba85ff36510ef44fd6451bac03b515aa4166
Bianca <beeankha@gmail.com>
2018-04-03 16:26:04 -0400

    Update Jokes.py
    Adding time breaks.

--- Jokes.py
+++ Jokes.py
@@ -1,11 +1,17 @@
+import time
+
+print('Ready for some jokes? Press "Enter" to continue.')
+input()
 print('What do you get when you cross a snowman with a vampire?')
 input()
 print('Frostbite!')
 print()
+time.sleep(1)
 print('What do dentists call an astronaut\'s cavity?')
 input()
 print('A black hole!')
 print()
+time.sleep(1)
 print('Knock, knock.')
 input()
 print("Who's there?")
