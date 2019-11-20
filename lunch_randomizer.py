1b02435c6ae65ff2eb4c517c9026633c887d327a
beeankha <beeankha@gmail.com>
2019-11-20 11:09:07 -0500

    Add new files

---
+++ lunch_randomizer.py
@@ -0,0 +1,21 @@
+import random
+
+lunch_list = ["Luna",
+              "Lucky's",
+              "Bull City Burger & Brewery",
+              "Neomonde",
+              "PokeWorks",
+              "M Kokko",
+              "Dashi",
+              "Toast",
+              "M Tempura",
+              "Copa",
+              "M Sushi",
+              "Taberna",
+              "Toro",
+              "Pie Pushers",
+              "Mateo's",
+              ]
+print("Here's where you should go to lunch today: \n",
+    random.choice(lunch_list))
+
