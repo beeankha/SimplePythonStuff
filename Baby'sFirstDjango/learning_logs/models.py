c1f6e0a273f9cd2e88a126d53fcb103e02a7e980
beeankha <beeankha@gmail.com>
2020-03-15 17:26:35 -0400

    Restrict regular users' access to topics that they did not create

--- Baby'sFirstDjango/learning_logs/models.py
+++ Baby'sFirstDjango/learning_logs/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Topic(models.Model):
     """A topic the user is learning about!"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Return a string representation of the model."""
