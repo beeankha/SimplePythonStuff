5fc7b11ab4249b17433b0316fc4086e08d8916d4
beeankha <beeankha@gmail.com>
2021-03-12 15:12:32 -0500

    Add earthquake data mapping files

---
+++ Baby'sFirstDataScience/eq_world_map.py
@@ -0,0 +1,46 @@
+import json 
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+# The offline module renders the map
+
+# Explore the structure of the data.
+filename = 'data/eq_data_30_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+    # The json.load() function converts the data into a format Python can work 
+    # with: in this case, a giant dictionary.
+
+all_eq_dicts = all_eq_data['features']
+# print(len(all_eq_dicts))
+
+mags, lons, lats, hover_texts = [], [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+    hover_texts.append(title)
+
+# Map the earthquakes.
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'text': hover_texts,
+    'marker': {
+        'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Hot',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'},
+    },
+}]
+# Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
