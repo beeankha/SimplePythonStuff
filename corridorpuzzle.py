7f5abf87cc389953bb9b2df1a98eb09c681fb37e
Bianca Henderson <biancahenderson@Biancas-MBP.attlocal.net>
2022-01-06 08:27:04 -0500

    Update README to include Corridor Puzzle

--- corridorpuzzle.py
+++ corridorpuzzle.py
@@ -6,9 +6,9 @@ doors = [False] * 101  # ignore index 0
 for i in range(1, 101):
     # for the second pass, x = 2, so we start at door 2, for the 3rd pass we start at door 3 etc.
     for j in range(i, 101, i):
-        doors[j] = not doors[j]  # using `not` to invert the Boolean value
+        doors[j] = not doors[j]
 
-# Print out just the positions of the open doors
+# This bit prints out just the positions of the open doors
 for i in range(1, 101):
     if doors[i]:
         print(i)
