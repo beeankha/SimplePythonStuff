56778c7c800d5f45d1ca35542ed4a816ac0a5aaf
Bianca Henderson <biancahenderson@Biancas-MBP.attlocal.net>
2022-01-05 22:39:58 -0500

    Add Corridor Puzzle file

---
+++ corridorpuzzle.py
@@ -0,0 +1,17 @@
+# True = open
+# False = closed
+
+doors = [False] * 101  # ignore index 0
+
+for i in range(1, 101):
+    # for the second pass, x = 2, so we start at door 2, for the 3rd pass we start at door 3 etc.
+    for j in range(i, 101, i):
+        doors[j] = not doors[j]  # using `not` to invert the Boolean value
+
+# Print out just the positions of the open doors
+for i in range(1, 101):
+    if doors[i]:
+        print(i)
+
+print(doors)
+print(doors.count(True))
